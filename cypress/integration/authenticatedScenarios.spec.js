097f9401843f9f407843fff80199c667f90dc7f0
Jairo Almeida <jairoam85@gmail.com>
2022-03-19 16:19:56 -0300

    Configure cypress grep

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -5,7 +5,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
     cy.login()
   })
 
-  it('CRUDs a note', () => {
+  it('CRUD a note', () => {
     const faker = require('faker')
     const noteDescription = faker.lorem.words(4)
 
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,5 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  //require('cypress-grep/src/plugin')(config)
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  require('cypress-grep/src/plugin')(config)
 }
